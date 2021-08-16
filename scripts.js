ecfa2a0a664ff7148603ced65f565453c4aadb42
emilywilbur <ewilbur94@gmail.com>
2021-08-16 15:22:30 -0400

    finished scrolling

--- scripts.js
+++ scripts.js
@@ -73,10 +73,19 @@ sr.reveal('.about__social-icon', {delay: 800, interval: 200})
 
 // SCROLL SKILLS
 
-
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', {distance: '20px', delay: 50, interval:100})
+sr.reveal('.skills__img', {delay:400})
 
 
 // SCROLL PORTFOLIO
 
+sr.reveal('.portfolio__img', {interval: 200})
+
 
 // SCROLL CONTACT
+
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__input', {delay: 400})
+sr.reveal('.contact__button', {delay: 600})
