c30985e4bdeb5d114f06cbcf6e246ed8a88ef15f
emilywilbur <ewilbur94@gmail.com>
2021-08-16 14:55:50 -0400

    scroll reveal animation for HOME

--- scripts.js
+++ scripts.js
@@ -49,10 +49,18 @@ function scrollActive() {
 
 
 // SCROLL REVEAL ANIMATION
-
+const sr = ScrollReveal ({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
 
 // SCROLL HOME
 
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', {delay: 200})
+sr.reveal('.home__img', {origin:'right', delay: 400})
 
 
 // SCROLL ABOUT
@@ -63,3 +71,6 @@ function scrollActive() {
 
 
 // SCROLL PORTFOLIO
+
+
+// SCROLL CONTACT
